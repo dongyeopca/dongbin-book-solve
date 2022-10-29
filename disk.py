2d66a1c32f14f7638327fffd627c9aa3fd828df0
dongyeopca <ckehdduq95@gmail.com>
2022-10-29 17:20:27 +0900

    fix: ds 2022 homework #15

--- disk.py
+++ disk.py
@@ -1,76 +1,117 @@
-# write fa sa 남은 공간이 sa보다 작으면 거절되고 "diskfull"출력 아니면 
-#한덩어리로 할수있으면 가장 앞쪽 안되면 쪼개서 채워넣기
-#delete fa
-# show fa 시작 위치를 출력/ 쪼개져있으면 모든 시작위치 출력
-# compact
 
-#["시작","끝","자료"]
+size = int(input())
 
+disk = [[1,size+1,"free"]]
 
-v = int(input())
-disk = [[1,v+1,"free"]]
-free_table= [[1,v+1]]
-free_size = v
 while True:
     disk.sort()
     order = input().split()
     if order[0]=="end":
         break
     if order[0]=="write":
-        order[-1]=int(order[-1])
-        size = order[-1]
-        if free_size<size:
+        for i in disk:
+            if i[-1]==order[1]:
+                print("error")
+                break
+        if size<int(order[-1]):
             print("diskfull")
             continue
-        free_size-=size
-        for i in range(len(disk)):
-            if disk[i][-1]=="free":
-                if disk[i][1]-disk[i][0]>size:#여유공간보다 작으면
-                    disk.append([disk[i][0],disk[i][0]+size,order[1]])
-                    disk.append([disk[i][0]+size,disk[i][1],"free"])
-                    disk.pop(i)
-                    break
-                elif disk[i][1]-disk[i][0] == size:
-                    disk[i][-1]=order[1]
-                    break
-                else:
-                    disk[i][-1]=order[1]
-                    size-=disk[i][1]-disk[i][0]
 
-    elif order[0]=="delete":
-        flag = False
-        for i in disk:
-            if i[-1]==order[-1]:
-                flag = True
-                i[-1]="free"
-                free_size+=i[1]-i[0]
-        if not flag:
-            print("error")
+        order_size = int(order[-1])
 
-    elif order[0]=="show":
-        flag = False
+        size -= order_size
+        #한번에 넣을수 있는공간 탐색
+        flag = True
         for i in disk:
-            if i[-1]==order[-1]:
-                flag = True
-                print(i[0]-1,end=" ")
+            node_size = i[1]-i[0]
+            if i[-1]=="free":
+                if node_size>order_size:
+                        flag = False
+                        i[-1] = order[1]
+                        i[1] = i[0]+order_size
+                        disk.append([i[1],i[1]+node_size-order_size,'free'])
+                        break
+                elif node_size==order_size:
+                    flag = False
+                    i[-1] = order[1]
+                    break
+        #아니면
         if flag:
+            for i in disk:
+                node_size = i[1]-i[0]
+                if i[-1]=="free":
+                    if node_size>order_size:
+                        i[-1] = order[1]
+                        i[1] = i[0]+order_size
+                        disk.append([i[1],i[1]+node_size-order_size,'free'])
+                        break
+                    elif node_size==order_size:
+                        i[-1] = order[1]
+                        break
+                    else:
+                        i[-1] = order[1]
+                        order_size-=node_size
+                        continue
+    elif order[0]=="show":
+        flag = True
+        for i in disk:
+            if i[-1]==order[1]:
+                flag = False
+                print(i[0]-1,end=" ")
+        if not flag:
             print()
         else:
             print("error")
-    elif order[0]=="compact":
-        index = 0
-        for i in range(len(disk)):
-            if disk[index][-1]=="free":
-                disk.pop(index)
-            else:
-                if index!=0:
-                    if disk[index-1][-1]==disk[index][-1]:
-                        disk[index-1][1]=disk[index-1][1]+(disk[index][1]-disk[index][0])
-                        disk.pop(index)
-                    else:
-                        disk[index] = [disk[index-1][1],disk[index-1][1]+disk[index][1]-disk[index][0],disk[index][-1]]
-                        index+=1
+
+    elif order[0]=="delete":
+        flag = True
+        for i in disk:
+            if i[-1]==order[1]:
+                flag = False
+                i[-1]='free'
+                size+=i[1]-i[0]
+        stack = []
+        for i in disk:
+            if stack:
+                if stack[-1][-1]==i[-1]:
+                    stack[-1][1]=i[1]
                 else:
-                    disk[index] = [1,1+disk[0][1]-disk[0][0],disk[0][-1]]
-                    index+=1
-        disk.append([disk[-1][1],disk[-1][1]+free_size,"free"])
+                    stack.append(i)
+            else:
+                stack.append(i)
+        disk = stack
+        if flag:
+            print("error")
+
+    elif order[0]=="compact":
+        stack = []
+        for i in disk:
+            if stack:
+                if stack[-1][-1]==i[-1]:
+                    stack[-1][1]=i[1]
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+        #free제거
+        index = 0
+        while index != len(stack):
+            if stack[index][-1]=="free":
+                stack.pop(index)
+            else:
+                index+=1
+        #재정렬
+        if stack:
+            disk = []
+            for i in stack:
+                if disk:
+                    if disk[-1][-1]==i[-1]:
+                        disk[-1][1]+=i[1]-i[0]
+                    else:
+                        disk.append([disk[-1][1],disk[-1][1]+i[1]-i[0],i[-1]])
+                else:
+                    disk.append([1,i[1]-i[0]+1,i[-1]])
+            disk.append([disk[-1][1],disk[-1][1]+size,'free'])
+        else:
+            disk = [[1,size+1,'free']]
+
