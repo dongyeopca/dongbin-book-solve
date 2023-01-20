fb1954cca5de4bb6dfbc8bf5994fee354a37296c
dongyeopca <ckehdduq95@gmail.com>
2023-01-20 14:39:15 +0900

    algorithm: stack/queue

---
+++ 프로그래머스/택배상자.js
@@ -0,0 +1,29 @@
+function solution(order) {
+  var answer = 0;
+  const stack = [];
+  let box = 1;
+  for (let value of order) {
+    let flag = false;
+    while (true) {
+      if (stack.length == 0) {
+        stack.push(box++);
+      }
+      if (value > stack.at(-1)) {
+        stack.push(box++);
+      } else if (value == stack.at(-1)) {
+        stack.pop();
+        answer += 1;
+        flag = true;
+        break;
+      } else {
+        console.log('?');
+        break;
+      }
+    }
+    if (!flag) {
+      return answer;
+    }
+  }
+
+  return answer;
+}
