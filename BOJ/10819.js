c74606823514ceb9dfc3b937212918b647dccc89
dongyeopca <ckehdduq95@gmail.com>
2023-01-27 14:40:58 +0900

    algorithm: permutation

---
+++ BOJ/10819.js
@@ -0,0 +1,35 @@
+let fs = require('fs');
+let dev = false;
+let input = fs
+  .readFileSync(dev ? './test.txt' : './dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+let n = Number(input[0]);
+let a = input[1].split(' ').map(Number);
+
+const makePermutation = function (arr, number) {
+  const results = [];
+  if (number == 1) return arr.map((e) => [e]);
+
+  arr.forEach((value, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutation = makePermutation(rest, number - 1);
+    const attached = permutation.map((e) => [value, ...e]);
+    results.push(...attached);
+  });
+  return results;
+};
+
+console.log(
+  Math.max(
+    ...makePermutation(a, n).map((e) => {
+      let value = 0;
+      for (let i = 2; i <= n; i++) {
+        value += Math.abs(e[i - 2] - e[i - 1]);
+      }
+      return value;
+    })
+  )
+);
