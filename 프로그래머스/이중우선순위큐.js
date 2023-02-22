901e184ca2e2df520dcc2a3dbdcfe7567f0280a0
dongyeopca <ckehdduq95@gmail.com>
2023-02-22 13:13:46 +0900

    algorithm: q

--- 프로그래머스/이중우선순위큐.js
+++ 프로그래머스/이중우선순위큐.js
@@ -1,24 +1,43 @@
+// function solution(operations) {
+//   var answer = [];
+//   for (const item of operations) {
+//     const [order, number] = item.split(" ");
+//     if (order === "I") {
+//       answer.push(number);
+//       answer.sort((a, b) => a - b);
+//       continue;
+//     }
+//     if (order === "D") {
+//       if (number === "1") {
+//         answer.pop();
+//       } else {
+//         answer.shift();
+//       }
+//     }
+//   }
+//   if (answer.length == 0) {
+//     answer = [0, 0];
+//   } else {
+//     answer = [Math.max(...answer), Math.min(...answer)];
+//   }
+//   return answer;
+// }
 function solution(operations) {
-  var answer = [];
-  for (const item of operations) {
-    const [order, number] = item.split(" ");
-    if (order === "I") {
-      answer.push(number);
-      answer.sort((a, b) => a - b);
-      continue;
-    }
-    if (order === "D") {
-      if (number === "1") {
-        answer.pop();
-      } else {
-        answer.shift();
-      }
+  let q = [];
+  for (const oper of operations) {
+    let [order, number] = oper.split(' ');
+    number = parseInt(number);
+    switch (order) {
+      case 'I':
+        q.push(number);
+        q.sort((a, b) => a - b);
+        break;
+      case 'D':
+        if (!q.length) continue;
+        number == -1 ? q.shift() : q.pop();
+        break;
     }
   }
-  if (answer.length == 0) {
-    answer = [0, 0];
-  } else {
-    answer = [Math.max(...answer), Math.min(...answer)];
-  }
-  return answer;
+  if (q.length) return [q[q.length - 1], q[0]];
+  return [0, 0];
 }
